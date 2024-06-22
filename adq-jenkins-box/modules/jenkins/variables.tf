97c237eecb5825671ee0d66743fabd1bb07fd4bf
Saravana <67310667+SaravanaNani@users.noreply.github.com>
2024-06-22 18:07:49 +0530

    Create variables.tf

---
+++ adq-jenkins-box/modules/jenkins/variables.tf
@@ -0,0 +1,103 @@
+variable "instance_name" {
+  type = string
+}
+
+variable "machine_type" {
+  type = string
+}
+
+variable "zone" {
+  type = string
+}
+
+variable "image" {
+  type = string
+}
+
+variable "label_name" {
+  description = "The label is created for server-label."
+  type        = string
+}
+
+variable "app_label" {
+  description = "The label is created for application in the VM."
+  type        = string
+}
+
+variable "server_name_label" {
+  description = "The label is created for server-name."
+  type        = string
+}
+
+variable "env_label" {
+  description = "The label is created for environment-name."
+  type        = string
+}
+
+variable "project_id" {
+  type = string
+}
+
+variable "vpc_name" {
+  type = string
+}
+
+variable "jenkins_server_subnet_name" {
+  type = string
+}
+
+variable "jenkins_server_region" {
+  type = string
+}
+
+variable "jenkins_server_cidr" {
+  type = string
+}
+
+variable "desktop_server_subnet_name" {
+  type = string
+}
+
+variable "desktop_server_region" {
+  type = string
+}
+
+variable "desktop_server_cidr" {
+  type = string
+}
+
+variable "jenkins_server_firewall_name" {
+  type = string
+}
+
+variable "jenkins_server_network_tags" {
+  type = string
+}
+
+variable "desktop_server_firewall_name" {
+  type = string
+}
+
+variable "desktop_server_network_tags" {
+  type = string
+}
+
+variable "tomcat_server_firewall_name" {
+  type = string
+}
+
+variable "tomcat_server_network_tags" {
+  type = string
+}
+
+variable "custom_svc_account_id" {
+  type = string
+}
+
+variable "custom_svc_display_name" {
+  type = string
+}
+
+variable "roles" {
+  type = list(string)
+}
